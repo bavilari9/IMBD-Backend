52bcea2cf502f50048a56a2632ab652aca63c6f3
Oscar David Díaz <oscar.diaz@helloiconic.com>
2020-01-21 17:13:48 -0600

    Early Access Code

--- api/controllers/Codes.js
+++ api/controllers/Codes.js
@@ -1,19 +1,40 @@
+const CryptoJS = require("crypto-js");
 const Codes = require ('../models/Codes'),
-        router = require('express').Router()
+router = require('express').Router()
 
 
-
-        router.get('/', (req, res) => {
-            // require the code from the front end 
-            let code = req.body.code
-            Codes
-               .findCode(code)
-               .then(data => { // once we get them back
-                     res.json(data)
-               })
-               .catch((err) => {
-                 console.log('Profile Get Controller Index Error', err)
-                })
-            })
+router.get('/checkCode', (req, res) => {
+  console.log("cookies", req.cookies)
+  // res.clearCookie('early-access-code');
+  res.json({
+    exists: Boolean(req.cookies && req.cookies['early-access-code'])
+  });
+});
+router.get('/', (req, res) => {
+  
+  // require the code from the front end 
+  let code = req.query.code;
+  Codes
+  .findCode(code)
+  .then(data => { // once we get them back
+    console.log("@data", data)
+    if (data && data.length > 0) {
+      const cipherCode = CryptoJS.AES.encrypt(code, 'SECRET_KEY');
+      res.setHeader('Cache-Control', 'private');
+      res.cookie("early-access-code", cipherCode.toString())
+      res.json({
+        valid: true,
+        hashCode: cipherCode.toString()
+      });
+    } else {
+      res.json({
+        valid: false
+      })
+    }
+  })
+  .catch((err) => {
+    console.log('Profile Get Controller Index Error', err)
+  })
+})
 
 module.exports = router;
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 8080
 const cors = require('cors');
 const Auth = require('./services/auth')
 const logger = require('morgan');
+var cookieParser = require('cookie-parser')
 
 
 // cross origin request 
@@ -16,6 +17,7 @@ app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
+app.use(cookieParser());
 
 // body parser 
 app.use(bodyParser.urlencoded({extended:false}))
@@ -23,6 +25,26 @@ app.use(bodyParser.json());
 
 // logger to see whats going on
 app.use(logger('dev'));
+
+// app.use('/', (req,res, next) => {
+//     console.log("test", req.cookies);
+    
+//     next();
+// });
+// app.use(function (req, res, next) {
+//     // Website you wish to allow to connect
+//     res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
+//     // Request methods you wish to allow
+//     res.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     // Request headers you wish to allow
+//     res.set('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.set('Access-Control-Allow-Credentials', true);
+//     // Pass to next layer of middleware
+//     next();
+// });
+
 
 // before all routes, use the middleware we define in Auth to get the
 // current user, or i can put it directy when requiring the controller 
@@ -35,7 +57,6 @@ app.use('/search', require('./api/controllers/Search'));
 app.use('/email', require('./api/controllers/Email'));
 app.use('/codes', require('./api/controllers/Codes'))
 app.use('/login', require('./api/controllers/sessions'));
-
 
 
 app.listen(PORT, ()=> console.log('Server listening on port ❤ 💩', PORT))
